102995338cbf9abca717eaefe93bb68a9eb12fd2
alexpodlucky <app5775@psu.edu>
2020-09-04 01:56:07 +0000

    errors

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ c2 = float(f1-32)*5/9
 
 unit = input("Enter unit in F/f or C/c: ")
 if unit == "C" or unit == "c" :
-  print(f"{c}° in Celsius is equivalent to {f}° Farenheit. ");
+  print(f"{c}° in Celsius is equivalent to {f}° Fahrenheit. ");
 elif unit == "F" or unit == "f" :
-  print(f"{f1}° in Farenheit is equivalent to {c2}° Celsius." );
+  print(f"{f1}° in Fahrenheit is equivalent to {c2}° Celsius." );
 else:
   print(f"Invalid unit({unit}).")
 
